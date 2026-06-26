6a187923468df9c896e6c5908fc2f14e5ff8849e
Praveen Kumar <praveenkumar1782o@gmail.com>
2026-06-26 16:51:46 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Classic-Checkers/',
   plugins: [react()],
   server: {
     allowedHosts: ['fresh-pillows-kick.loca.lt']
